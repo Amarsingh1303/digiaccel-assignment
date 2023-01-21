b18ce59f756f906b1344505dfc64e21967cc15d7
amarsingh1303 <amarsingh1303@gmail.com>
2023-01-21 12:27:14 +0530

    fix path error

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/userRoutes");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
