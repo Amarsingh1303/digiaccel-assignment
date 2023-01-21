ad497f3330a200f31df79e8db7cae9cdec50f733
amarsingh1303 <amarsingh1303@gmail.com>
2023-01-21 11:47:19 +0530

    deployment settings

--- server/index.js
+++ server/index.js
@@ -22,6 +22,9 @@ mongoose.connection.on("error", () => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.use(userRouter);
